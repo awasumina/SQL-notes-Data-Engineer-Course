b9c954b353f0461d0ca3f38eacafa09018412dc0
sumina <suminaawa123@gmail.com>
2024-08-06 17:52:15 +0545

    Keywords

--- 01-keywords I/count.sql
+++ 01-keywords I/count.sql
@@ -9,7 +9,7 @@ INSERT INTO people (id, name, birthdate, deathdate) VALUES
 (6, 'A.J. Langer', '1974-05-22', NULL),
 (7, 'Aaliyah', '1979-01-16', '2001-08-25'),
 (8, 'Aaron Ashmore', '1979-10-07', NULL),
-(9, 'Aaron Hann', NULL, NULL),
+(9, 'Aaron Hann', NULL, NULL), 
 (10, 'Aaron Hill', '1983-04-23', NULL),
 (11, 'Aaron Hughes', NULL, NULL),
 (12, 'Aaron Kwok', '1965-10-26', NULL),
