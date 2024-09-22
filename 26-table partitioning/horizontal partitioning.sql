6f7370800d8ea745641cafe57f397296833327bd
sumina <suminaawa123@gmail.com>
2024-09-22 20:16:03 +0545

    Table Partitioning

---
+++ 26-table partitioning/horizontal partitioning.sql
@@ -0,0 +1,47 @@
+-- Create a new table film_descriptions containing 2 fields: film_id, which is of type INT, and long_description, which is of type TEXT.
+-- Occupy the new table with values from the film table.
+-- Create a new table called film_descriptions
+
+CREATE TABLE film_descriptions (
+    film_id INT,
+    long_description TEXT
+);
+
+-- Copy the descriptions from the film table
+INSERT INTO film_descriptions
+SELECT film_id, long_description FROM film;
+    
+-- Drop the descriptions from the original table
+ALTER TABLE film
+DROP COLUMN long_description;
+
+
+-- Join to view the original table
+SELECT * FROM film 
+JOIN film_descriptions USING(film_id);
+
+
+
+
+
+-- Create the table film_partitioned, partitioned on the field release_year.
+-- Create a new table called film_partitioned
+-- Create a new table called film_partitioned
+CREATE TABLE film_partitioned (
+  film_id INT,
+  title TEXT NOT NULL,
+  release_year TEXT
+)
+PARTITION BY LIST (release_year);
+
+-- Create the partitions for 2019, 2018, and 2017
+CREATE TABLE film_2019
+	PARTITION OF film_partitioned FOR VALUES IN ('2019');
+
+CREATE TABLE film_2018
+	PARTITION OF film_partitioned FOR VALUES IN ('2018');
+
+CREATE TABLE film_2017
+	PARTITION OF film_partitioned FOR VALUES IN ('2017');
+
+
