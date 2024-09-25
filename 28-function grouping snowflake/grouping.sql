c41868ac31c018dc284eedac7ab4bb2229a71f7e
sumina <suminaawa123@gmail.com>
2024-09-25 21:16:58 +0545

    Function grouping snowflake

--- 28-function grouping snowflake/grouping.sql
+++ 28-function grouping snowflake/grouping.sql
@@ -6,7 +6,7 @@
 SELECT order_id, pizza_id, sum(quantity) AS total_quantity
 FROM order_details
 -- GROUP the orders using group by all having total_quantity greater than 3
-GROUP BY ALL
+GROUP BY ALL      
 having total_quantity>3
 -- ORDER BY order id and total quantity in descending order
 order by order_id , total_quantity desc
